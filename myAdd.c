160fc0ad51d8cfefd75bb5ac0ef38b04ed62b898
Incitatous <dcianberlin@gmail.com>
2017-01-27 22:34:05 -0800

    fixed output

--- myAdd.c
+++ myAdd.c
@@ -9,26 +9,17 @@
 void myAdd(stack_t **stack, unsigned int ln)
 {
 	stack_t *tmp, *cur, *pre;
-	int i = 0;
 	int sum;
 
 	tmp = *stack;
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		++i;
-	}
-	if (i < 2)
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		printf("L%u: can't add, stack too short\n", ln);
 		exit(EXIT_FAILURE);
 	}
-	cur = pre = *stack;
-	if (cur->next != NULL)
+	else
 	{
-		cur = cur->next;
-		sum = (pre->n) + (cur->n);
+		tmp->next->n += tmp->n;
+		myPop(stack, ln);
 	}
-	/* testing */
-	printf("sum: %d\n", sum);
 }
